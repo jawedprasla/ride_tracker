8edd9ba91a03abf355fdea48a7f2ef050d7a4aa1
Jawed PRasla <jawedprasla@outlook.com>
2019-02-13 22:36:06 -0500

    After completing Spring JDBC course and added new files

---
+++ src/main/java/com/pluralsight/util/ServerError.java
@@ -0,0 +1,32 @@
+package com.pluralsight.util;
+
+public class ServerError {
+	private int code;
+	private String message;
+
+	public ServerError() {
+
+	}
+
+	public ServerError(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
